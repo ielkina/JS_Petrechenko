d509d342e8f08c8e5434979b1f02fbb93413e02f
Iryna <ielkinairinka@gmail.com>
2022-02-15 12:11:47 +0300

    +

--- js/34scr.js
+++ js/34scr.js
@@ -1,3 +1,26 @@
 // console.log(document.body);
 // console.log(document.head);
-console.log(document.documentElement);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
+
+// console.log(document.querySelector('#current'));
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
+// обязательно следует следить за тем что б кавычки не повторялись
+
+// console.log(document.querySelector('[data-current="3"'));
+// console.log(document.querySelector('[data-current="3"]').nextSibling);
+// console.log(document.querySelector('[data-current="3"]').previousSibling);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+
+for (let node of document.body.childNodes) {
+	if (node.nodeName == '#text') {
+		continue;
+	}
+	console.log(node);
+}
+
